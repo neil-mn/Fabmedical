352eb8c3695b0bc4e3f43dacc9c24946c144e565
neil-mn <neil.howell@mn.catholic.edu.au>
2022-09-16 04:58:05 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const request = require('request');
 
 const app = express();
+const appInsights = require("applicationinsights");
+appInsights.setup("574011f4-2243-4ba7-8366-89c826ecf293");
+appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
